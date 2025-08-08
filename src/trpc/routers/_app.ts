2a96efddc079b68f25995e2b039149f59cbef94c
yacinesekhri <136460089+yacinesekhri@users.noreply.github.com>
2025-08-08 20:10:20 +0100

    minor fixes

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,7 +5,6 @@ import { productsRouter } from '@/modules/products/server/procedures';
 import { tagsRouter } from '@/modules/tags/server/procedures';
 import { tenantsRouter } from '@/modules/tenants/server/procedures';
 import { checkoutRouter } from '@/modules/checkout/server/procedures';
-import { Library } from 'lucide-react';
 import { libraryRouter } from '@/modules/library/server/procedures';
 export const appRouter = createTRPCRouter({
   auth: authRouter,
